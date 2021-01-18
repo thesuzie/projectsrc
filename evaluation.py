189d1adde1e05fc92e176344f59ad4d5beb10ce1
suzie welby <sgw36@cam.ac.uk>
2021-01-18 15:13:56 +0000

    added comments and moved around code for combining annotations

--- evaluation.py
+++ evaluation.py
@@ -4,20 +4,46 @@ import numpy as np
 
 
 def combine_annotations(file1, file2):
-    new = file1
-    new['Label 1'] = file2['Label']
-    new['Sentiment 1'] = file2['Sentiment']
+    # INPUT: file1, file2 dataframes
+    # OUTPUT: new dataframe with labels from file 2 in label1 sent1 columns and final columns
+    combined = file1
+    combined['Label 1'] = file2['Label']
+    combined['Sentiment 1'] = file2['Sentiment']
 
-    return new
+    final_l = [0] * len(file1['Label'])
+    final_s = [0] * len(file1['Label'])
+    i = 0
+    while i < len(file1['Label']):
+        if combined['Label'][i] == combined['Label 1'][i]:
+            final_l[i] = combined['Label'][i]
+        else:
+            final_l[i] = "X"
+
+        if combined['Sentiment'][i] == combined['Sentiment 1'][i]:
+            final_s[i] = combined['Sentiment'][i]
+        else:
+            final_s[i] = "X"
+        i += 1
+
+    combined["Final Label"] = final_l
+
+    combined['Final Sentiment'] = final_s
+
+    return combined
+
 
 def count_labels(txt):
-
+    # INPUT: dataframe with label column
+    # OUTPUT: NONE, func prints answer
     (unique, counts) = np.unique(txt['Label'], return_counts=True)
     print('label, Count of labels')
     print(np.asarray((unique, counts)).T)
+    return None
+
 
 def count_sent(txt):
-
+    # INPUT: dataframe with sentiment column
+    # OUTPUT: NONE, func prints answer
     (unique, counts) = np.unique(txt['Sentiment'], return_counts=True)
     print('label, Count of Sentiment')
     print(np.asarray((unique, counts)).T)
@@ -34,29 +60,6 @@ count_labels(file2)
 count_sent(file2)
 
 combined = combine_annotations(file1, file2)
-
-# combine these into a function of only one loop
-final_l = [0] * len(file1['Label'])
-final_s = [0] * len(file1['Label'])
-i = 0
-while i < len(file1['Label']):
-    if combined['Label'][i] == combined['Label 1'][i]:
-        final_l[i] = combined['Label'][i]
-    else:
-        final_l[i] = "X"
-
-    if combined['Sentiment'][i] == combined['Sentiment 1'][i]:
-        final_s[i] = combined['Sentiment'][i]
-    else:
-        final_s[i] = "X"
-       # print("x")
-    #print(i)
-    i+=1
-
-
-combined["Final Label"] = final_l
-
-combined['Final Sentiment'] = final_s
 
 combined.to_csv('./data/data1combined.csv')
 
