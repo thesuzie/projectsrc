6c3c8a55d44a525633a18062d0b249ac7a36313a
suzie welby <sgw36@cam.ac.uk>
2021-01-20 15:03:53 +0000

    nn token indices encoding started

--- encode_simple.py
+++ encode_simple.py
@@ -1,5 +1,5 @@
 import re
-
+import pandas as pd
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
@@ -55,3 +55,77 @@ def tfidf_encode(txt):
     tfidf = vectorizer.fit_transform(txt['Content Cleaned'])
 
     return tfidf
+
+def nn_tokenize(content):
+    # content is a string
+
+    # remove links
+    links = "(http\S*)|(\S*\.co\S*)|(\S*\.net\S*)"
+    cleaned = re.sub(links, "", content)
+
+    # remove usernames and emails (strings containing @)
+    usrnm = "\S*@\S*"
+    cleaned = re.sub(usrnm, "", cleaned)
+
+    # remove unneeded indicator words that have been added
+    ignore = "(Start contract)|(Code \[.*\])|(Spoiler \(Click to View\))"
+    cleaned = re.sub(ignore, "", cleaned)
+
+    # remove punctuation and numbers
+    cleaned = re.sub('[^a-zA-Z]+', " ", cleaned)
+
+    # make lower case
+    clean_content = cleaned.lower()
+
+    # tokenize
+    tokens = word_tokenize(clean_content)
+
+    # lemmatize - can try with and without this
+    #lemmatizer = WordNetLemmatizer()
+    #lemmas = [lemmatizer.lemmatize(t) for t in tokens]
+
+    # remove stopwords
+    #remove_sw = [w for w in lemmas if not w in stopwords.words()]
+
+    return tokens
+
+
+def create_token_vocab(train):
+    token_vocab = set()
+    for content in train["Content"]:
+        for token in nn_tokenize(content):
+            token_vocab.add(token)
+
+    vocab = list(token_vocab)
+    return vocab
+
+
+def token_index(tok, vocab):
+    oov = len(vocab)
+    ind = tok
+    if not pd.isnull(tok):  # new since last time: deal with the empty lines which we didn't drop yet
+        if tok in vocab:  # if token in vocabulary
+            ind = vocab.index(tok)
+        else:  # else it's OOV
+            ind = oov
+    return ind
+
+def indices(content, vocab):
+    tokens = nn_tokenize(content)
+    tokinds = [token_index(t, vocab) for t in tokens]
+    return tokinds
+
+
+def nn_encode(train, test):
+
+    vocab = create_token_vocab(train)
+    train["Token_indices"] = [indices(content, vocab) for content in train["Content"]]
+
+    test["Token_indices"] = [indices(content, vocab) for content in test["Content"]]
+
+
+
+
+
+
+
