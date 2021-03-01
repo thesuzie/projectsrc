f62817876824d248ac0894e3ae168f3a0a686fcd
suzie welby <sgw36@cam.ac.uk>
2021-03-01 13:01:49 +0000

    working on rnn , trying to get biases in as well

--- rnn.py
+++ rnn.py
@@ -3,9 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from tensorflow import keras
-
+import ast
 from encode import nn_encode, create_token_vocab
 from keras.preprocessing.sequence import pad_sequences
+from collections import Counter
 
 # values from the full (unbalanced) dataset
 seq_length = 273
@@ -61,21 +62,56 @@ def find_longest_sequence(inds, longest_seq):
 
 
 def create_padded(sequences):
-
+    #print(f"first sequence: {sequences[0]}")
     padded = pad_sequences(sequences, maxlen=seq_length, dtype='int32', padding='post',
                            truncating='post', value=pad_value)
 
     return padded
 
+def find_biases(labels):
+    all_labs = labels
+    label_count = Counter(all_labs)
+    total_labs = len(all_labs)
+    print(label_count)
+    print(total_labs)
+
+    # use this to define an initial model bias
+    initial_bias = [(label_count[1] / total_labs),
+                    (label_count[2] / total_labs), (label_count[3] / total_labs),(label_count[4] / total_labs), (label_count[5] / total_labs),
+                    (label_count[6] / total_labs), (label_count[7] / total_labs), (label_count[9] / total_labs)]
+    print('Initial bias:')
+    print(initial_bias)
+
+    return initial_bias
+
+
+#prep_indices()
 
 train_dataset = pd.read_csv("/Users/suziewelby/year3/compsci/project/src/test_train/rnn_train.csv")
 test_dataset = pd.read_csv("/Users/suziewelby/year3/compsci/project/src/test_train/rnn_test.csv")
 
-X_train = create_padded(train_dataset["Token Indices"].tolist())
-y_train = train_dataset["Label"]
 
-X_test = create_padded(test_dataset["Token Indices"])
+def convert_to_list(tokens):
+    i =0
+    l = len(tokens)
+    tok_list = [[0]] *l
+    #print(l)
+    while i < l:
+        tok_list[i] = ast.literal_eval(tokens[i])
+        i+=1
+    #print(tok_list[0])
+    return tok_list
+
+
+X_train = create_padded(convert_to_list(train_dataset["Token Indices"]))
+y_train = train_dataset["Label"]
+bias = find_biases(y_train)
+
+X_test = create_padded(convert_to_list(test_dataset["Token Indices"]))
 y_test = test_dataset["Label"]
+
+n_labs = 8
+
 
 model = tf.keras.Sequential([
 
@@ -85,20 +121,37 @@ model = tf.keras.Sequential([
         # Use masking to handle the variable sequence lengths
         mask_zero=True),
     tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64)),
-    tf.keras.layers.Dense(64, activation='relu'),
-    tf.keras.layers.Dense(1)
+    tf.keras.layers.Dense(n_labs, activation='relu', bias_initializer=tf.keras.initializers.Constant(bias))
+
 ])
 
 
 model.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
               optimizer=tf.keras.optimizers.Adam(1e-4),
-              metrics=['accuracy'])
+              metrics=METRICS)
 
-history = model.fit((X_train,y_train), epochs=10,
-                    validation_data=(X_test, y_test),
-                    validation_steps=30)
+
+early_stopping = tf.keras.callbacks.EarlyStopping(
+    monitor='accuracy', verbose=1, patience=10, mode='max', restore_best_weights=True)
+
+EPOCHS = 50
+BATCH_SIZE = 30
+
+print("fitting...")
+
+history = model.fit(X_train, y_train, epochs=EPOCHS,
+                    batch_size=BATCH_SIZE,
+                    callbacks=[early_stopping]
+                    )
+
+print("finished fitting")
+
+preds = np.argmax(model.predict(X_test), axis=-1)
+#flat_preds = [p for pred in preds for p in pred]
+print(Counter(preds))
 
 test_loss, test_acc = model.evaluate((X_test, y_test))
+
 
 print('Test Loss: {}'.format(test_loss))
 print('Test Accuracy: {}'.format(test_acc))
@@ -110,7 +163,7 @@ plt.ylim(None,1)
 plt.subplot(1,2,2)
 plot_graphs(history, 'loss')
 plt.ylim(0,None)
-
+plt.show()
 
 
 
